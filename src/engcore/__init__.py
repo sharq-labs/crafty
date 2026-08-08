537812c8c82bae3723d1a45eb833172202da3313
Kareem Saad Elsharkawy <108046720+sharq-labs@users.noreply.github.com>
2026-08-08 10:53:55 +0300

    Export V0.3.5 adaptive engine

--- src/engcore/__init__.py
+++ src/engcore/__init__.py
@@ -1,2 +1,11 @@
 from .models import Variable, DesignSpace, ExperimentResult
 from .engine import SmartExperimentEngine
+from .adaptive_stacked_engine_v035 import AdaptiveStackedGPBOEngineV035
+
+__all__ = [
+    "Variable",
+    "DesignSpace",
+    "ExperimentResult",
+    "SmartExperimentEngine",
+    "AdaptiveStackedGPBOEngineV035",
+]
