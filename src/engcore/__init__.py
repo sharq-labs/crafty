83fe9fd7278866d8aea29a14373666612140716c
Kareem Saad Elsharkawy <108046720+sharq-labs@users.noreply.github.com>
2026-08-08 11:25:09 +0300

    Keep optional V0.3.5 BoTorch engine out of base package imports

--- src/engcore/__init__.py
+++ src/engcore/__init__.py
@@ -1,11 +1,2 @@
 from .models import Variable, DesignSpace, ExperimentResult
 from .engine import SmartExperimentEngine
-from .adaptive_stacked_engine_v035 import AdaptiveStackedGPBOEngineV035
-
-__all__ = [
-    "Variable",
-    "DesignSpace",
-    "ExperimentResult",
-    "SmartExperimentEngine",
-    "AdaptiveStackedGPBOEngineV035",
-]
