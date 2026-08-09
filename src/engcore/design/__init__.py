dc2fb398e3eca747960056abc4ad45b4eba422e6
Kareem Saad Elsharkawy <108046720+sharq-labs@users.noreply.github.com>
2026-08-10 00:57:36 +0300

    D2: export mixed candidate generation contracts

--- src/engcore/design/__init__.py
+++ src/engcore/design/__init__.py
@@ -13,10 +13,30 @@ from .evaluation import (
     require_result_binding,
 )
 from .fidelity import FidelityLadder, FidelityRung
+from .generation import (
+    GENERATION_BINDING_METADATA_KEY,
+    CandidateGenerationBatch,
+    CandidateGenerationPlan,
+    CandidateProposal,
+    GenerationStrategy,
+    ProposalDecision,
+    ProposalGate,
+    ProposalRejection,
+    TwinMaterializer,
+    assignment_digest,
+    bind_generation_to_twin,
+    generate_initial_population,
+    generation_binding_payload,
+)
 from .population import DesignPopulation
+from .sampling import MixedVariableSampler
 from .space import DesignSpace, DesignSpaceReference
 
 __all__ = [
+    "GENERATION_BINDING_METADATA_KEY",
+    "CandidateGenerationBatch",
+    "CandidateGenerationPlan",
+    "CandidateProposal",
     "DesignCandidate",
     "DesignCandidateReference",
     "DesignEvaluation",
@@ -27,12 +47,22 @@ __all__ = [
     "FidelityLadder",
     "FidelityRung",
     "FidelitySelection",
+    "GenerationStrategy",
+    "MixedVariableSampler",
     "ParetoArchive",
     "ProjectedObjective",
+    "ProposalDecision",
+    "ProposalGate",
+    "ProposalRejection",
     "ResultBinding",
     "ScopedEliteArchive",
     "SelectionEligibility",
+    "TwinMaterializer",
+    "assignment_digest",
+    "bind_generation_to_twin",
     "dominates",
+    "generate_initial_population",
+    "generation_binding_payload",
     "project_objectives",
     "require_result_binding",
 ]
